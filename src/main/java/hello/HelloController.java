84075e56aeb1f31a236c3b926c554710649c3f86
Alessandro Berra <alessandro.berra@gmail.com>
2015-10-09 00:15:17 +0200

    rest consumer of quote fetcher

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,11 +1,13 @@
 package hello;
 
+import java.text.MessageFormat;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
@@ -30,6 +32,21 @@ public class HelloController {
 			@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template,
 				name));
+
+	}
+
+	@RequestMapping(value = "/fetch_quote")
+	public String fetchQuote() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		Quote quote = restTemplate.getForObject(
+				"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		String text = "<html><head></head><body><h1> {0} </h1><br/><h2> {1} </h2></body></html>";
+		String format = MessageFormat.format(text, quote.getType(), quote
+				.getValue().getQuote());
+
+		return format;
+
 	}
 
 }
