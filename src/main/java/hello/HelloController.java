d74c6596062f037b7287459685c3d1b8b6893b63
Alessandro Berra <alessandro.berra@gmail.com>
2015-10-08 23:44:28 +0200

    rest controller returning JSON object

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,15 +1,35 @@
 package hello;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController
-{
+public class HelloController {
 
-  @RequestMapping("/ciao")
-  public String index()
-  {
-    return "Sucate tutti!";
-  }
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/ciao")
+	public String index() {
+		return "Sucate tutti!";
+	}
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template,
+				name));
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/greeting2")
+	public Greeting greeting2(
+			@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template,
+				name));
+	}
+
 }
